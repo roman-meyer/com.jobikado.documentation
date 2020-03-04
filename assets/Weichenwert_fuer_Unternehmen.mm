cd90aea1db75987a899c5015ce9df3585168a008
Roman Meyer <roman.meyer@hsh-berlin.com>
2020-03-04 17:01:50 +0100

    assets hinzugefuegt

---
+++ assets/Weichenwert_fuer_Unternehmen.mm
@@ -0,0 +1,68 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1583230208960" ID="ID_737282587" MODIFIED="1583231887952" TEXT="Jobikado">
+<font NAME="SansSerif" SIZE="49"/>
+<node COLOR="#3366ff" CREATED="1583230208960" ID="ID_678158715" MODIFIED="1583231802449" POSITION="left" TEXT="Fachkraft A">
+<font BOLD="true" NAME="SansSerif" SIZE="14"/>
+<node COLOR="#3366ff" CREATED="1583230473471" ID="ID_1411353365" MODIFIED="1583232448178" TEXT="Arbeitgeber X ">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1583232140160" ID="ID_552160859" MODIFIED="1583232302683" TEXT="individueller Weichenwert">
+<node COLOR="#3366ff" CREATED="1583230380488" ID="ID_1896637991" MODIFIED="1583231595914" TEXT="aktuelles Gehalt">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#3366ff" CREATED="1583230403312" HGAP="18" ID="ID_1850329967" MODIFIED="1583231595913" TEXT="gew&#xfc;nschte Gehalt">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+</node>
+<node COLOR="#ff6666" CREATED="1583231080172" ID="ID_982268836" MODIFIED="1583231584920" POSITION="right">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font size="4">Unternehmensprofil</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+<font BOLD="true" NAME="SansSerif" SIZE="28"/>
+<node COLOR="#ff6666" CREATED="1583231099955" ID="ID_254574827" MODIFIED="1583231584906" TEXT="Durchschnittlicher Weichenwert">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#ff6666" CREATED="1583231115553" ID="ID_1880907058" MODIFIED="1583231584916" TEXT="Aktualisierungsdatum">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node COLOR="#3366ff" CREATED="1583230208960" ID="ID_332960022" MODIFIED="1583231818463" POSITION="left" TEXT="Fachkraft B">
+<font BOLD="true" NAME="SansSerif" SIZE="14"/>
+<node COLOR="#3366ff" CREATED="1583230473471" ID="ID_862039922" MODIFIED="1583232453160" TEXT="Arbeitgeber X">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1583232140160" ID="ID_712755887" MODIFIED="1583232290784" TEXT="individueller Weichenwert">
+<node COLOR="#3366ff" CREATED="1583230380488" ID="ID_821136837" MODIFIED="1583231595914" TEXT="aktuelles Gehalt">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#3366ff" CREATED="1583230403312" HGAP="18" ID="ID_745042255" MODIFIED="1583231595913" TEXT="gew&#xfc;nschte Gehalt">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+</node>
+<node COLOR="#3366ff" CREATED="1583230208960" ID="ID_1875870545" MODIFIED="1583231824993" POSITION="left" TEXT="Fachkraft C">
+<font BOLD="true" NAME="SansSerif" SIZE="14"/>
+<node COLOR="#3366ff" CREATED="1583230473471" ID="ID_967260300" MODIFIED="1583232460873" TEXT="Arbeitgeber X">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1583232140160" ID="ID_416701729" MODIFIED="1583232329456" TEXT="individueller Weichenwert">
+<node COLOR="#3366ff" CREATED="1583230380488" ID="ID_886180866" MODIFIED="1583231595914" TEXT="aktuelles Gehalt">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#3366ff" CREATED="1583230403312" HGAP="18" ID="ID_51367658" MODIFIED="1583231595913" TEXT="gew&#xfc;nschte Gehalt">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+</node>
+</node>
+</map>
